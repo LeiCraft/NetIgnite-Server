657d2e7e72aa72c011a5365aa7c3481551662990
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-05-23 11:38:55 +0200

    updated error page

--- error.vue
+++ error.vue
@@ -1,22 +1,25 @@
 <template>
-    <div class="container-xxl error-wrapper">
-        <h1 class="text-center">Error {{ (error as any).statusCode }}</h1>
+    <div class="app">
+        <!-- <Header /> -->
+        <main class="content">
+            <div class="container-xxl error-wrapper">
+                <h1 class="text-center">Error {{ (error as any).statusCode }}</h1>
+            </div>
+        </main>
+        <Footer />
     </div>
 </template>
 
 <script setup lang="ts">
 
 import type { NuxtError } from '#app';
+import Footer from '~/components/Footer.vue';
 
 const props = defineProps({
-  error: Object as () => NuxtError
+    error: Object as () => NuxtError
 })
 
 const handleError = () => clearError({ redirect: '/' })
-
-definePageMeta({
-    layout: 'default'
-});
 
 useSeoMeta({
     title: `Error ${(props.error as any).statusCode} | NetIgnite`,
@@ -27,9 +30,31 @@ useSeoMeta({
 
 <style scoped>
 
+.app {
+    font-family: "Rubik", sans-serif;
+    background-color: #0b0c1b;
+    color: white;
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+    word-wrap: break-word;
+    display: flex;
+    flex-direction: column;
+}
+
+.content {
+    margin-top: 6rem;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+
 .error-wrapper {
     margin: auto;
     max-width: 500px;
 }
-
 </style>
+
+
