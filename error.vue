3efa2536e4dc885c797b6143f624758d77299804
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-05-23 11:30:08 +0200

    updated error handling

---
+++ error.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container-xxl error-wrapper">
+        <h1 class="text-center">Error {{ (error as any).statusCode }}</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import type { NuxtError } from '#app';
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+const handleError = () => clearError({ redirect: '/' })
+
+definePageMeta({
+    layout: 'default'
+});
+
+useSeoMeta({
+    title: `Error ${(props.error as any).statusCode} | NetIgnite`,
+    description: 'An error occurred while processing your request.',
+});
+
+</script>
+
+<style scoped>
+
+.error-wrapper {
+    margin: auto;
+    max-width: 500px;
+}
+
+</style>
