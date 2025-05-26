93548b2c81da59b40a39fbd098d912416f7960b9
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-05-26 19:45:53 +0200

    added pwa

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+    <VitePwaManifest />
     <div class="app">
         <!-- <Header /> -->
         <main class="content">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
 
@@ -11,17 +10,17 @@ export default defineNuxtConfig({
     app: {
         head: {
             charset: "utf-8",
-			viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+            viewport: "width=device-width, initial-scale=1, maximum-scale=1",
             link: [
                 {
                     rel: 'stylesheet',
                     href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css'
                 },
                 { rel: "preconnect", href: "https://fonts.googleapis.com" },
-				{ rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
-				{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700;800;900&display=swap" },
+                { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+                { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700;800;900&display=swap" },
 
-				{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
             ],
             script: [
                 {
@@ -29,7 +28,7 @@ export default defineNuxtConfig({
                     tagPosition: 'bodyClose'
                 }
             ]
-            
+
         }
     },
 
@@ -43,5 +42,23 @@ export default defineNuxtConfig({
             websocket: true
         }
     },
+
+    modules: ['@vite-pwa/nuxt'],
+
+    pwa: {
+        registerType: 'autoUpdate',
+        manifest: {
+            name: 'NetIgnite',
+            short_name: 'NetIgnite',
+            start_url: '/',
+            description: 'NetIgnite - Power On Devices From Anywhere',
+
+            display: 'standalone',
+            orientation: 'portrait',
+
+            theme_color: '#ffffff',
+            background_color: '#0b0c1b',
+        }
+    }
 
 });
