5d3348fea18bec6e9143f2d23b7171bef2177ced
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-06-01 20:24:18 +0200

    added PUT /agents/:id api route

---
+++ server/api/agents/[id]/index.put.ts
@@ -0,0 +1,32 @@
+import { DBStorage } from "../../../db";
+
+type UpdatePayload = DBStorage.Agent.ModelWithoutID;
+
+export default defineEventHandler(async (event) => {
+
+    const userinfo = event.context.userinfo as UserAuthInfo;
+    if (!userinfo) return;
+
+    const payload = await readBody(event) as UpdatePayload | undefined;
+    if (
+        !payload ||
+        typeof payload.name !== "string" ||
+        typeof payload.description !== "string" ||
+        !["server", "microcontroller"].includes(payload.type) ||
+        typeof payload.secret !== "string"
+    ) {
+        setResponseStatus(event, 400);
+        return { status: "ERROR", message: "Invalid payload", data: null };
+    }
+
+    payload.ownerID = userinfo.userID;
+
+    const result = await DBStorage.Agents.insert(payload);
+    if (!result) {
+        setResponseStatus(event, 500);
+        return { status: "ERROR", message: "Failed to create Agent", data: null };
+    }
+
+    setResponseStatus(event, 201);
+    return { status: "OK", message: "Agent created successfully", data: result };
+});
