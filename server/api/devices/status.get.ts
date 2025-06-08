c2c1db71331ef2e845b9fa03677f27625d9bc197
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-06-08 11:35:41 +0200

    update

---
+++ server/api/devices/status.get.ts
@@ -0,0 +1,47 @@
+import type { UserAuthInfo } from '@/server/utils/auth/handler';
+import { AgentControlService } from '~/server/agent-control-service';
+import { DBStorage } from '~/server/db';
+import type { ModelUtils } from '~/utils/models/utils';
+
+export default defineEventHandler(async (event) => {
+
+    const userinfo = event.context.userinfo as UserAuthInfo;
+    if (!userinfo) return;
+
+    const idList = getQuery(event).ids?.toString().split(',').map(id => parseInt(id, 10)).filter(id => !isNaN(id));
+
+    const result: {
+        [id: string]: ModelUtils.OnlineStatus.Type
+    } = {};
+
+    const ownedDevices = await DBStorage.Devices.getAllByOwnerID(userinfo.userID);
+    if (!ownedDevices) {
+        setResponseStatus(event, 500);
+        return { status: "ERROR", message: "Failed to retrieve devices statuses.", data: null };
+    }
+
+    for (const device of ownedDevices) {
+
+        if (idList && !idList.includes(device.id)) {
+            continue;
+        }
+
+        const agent = AgentControlService.agents.get(device.agentID);
+        if (!agent) {
+            result[device.id.toString()] = "unknown";
+            continue;
+        }
+
+        const deviceStatus = await agent.sendCommand("GET_STATUS", {
+            address: device.macAddress,
+        });
+        if (deviceStatus?.status) {
+            result[device.id.toString()] = deviceStatus.status as ModelUtils.OnlineStatus.Type;
+        } else {
+            result[device.id.toString()] = "unknown";
+        }
+
+    }
+    
+    return { status: "OK", data: result};
+});
